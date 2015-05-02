e0243fdf54b59b0d72427411c0de37812d889af7
Kwon Tae Guk <xornrbboy@gmail.com>
2015-05-03 01:49:10 +0900

    Update db_config.php

--- mongle_board/db_config.php
+++ mongle_board/db_config.php
@@ -1,8 +1,8 @@
 <?php
 define('DB_SERVER_ADDR', 'localhost');
-define('DB_NAME', 'sgcsbrd');
-define('DB_USER_ID', 'sgcsbrduser');
-define('DB_USER_PW', 'zjarhdtlftp12');
+define('DB_NAME', '123123');
+define('DB_USER_ID', '123123');
+define('DB_USER_PW', '123123');
 
 define('SECURE', FALSE);
 ?>
